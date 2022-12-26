da031caff7a4006d9391b65f3a11ec16b06971c4
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2022-12-26 18:46:36 +0800

    1

--- handlers/handler.go
+++ handlers/handler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"runtime"
+	"strings"
 
 	"github.com/869413421/wechatbot/config"
 	"github.com/869413421/wechatbot/service"
@@ -58,11 +59,13 @@ func Handler(msg *openwechat.Message) {
 
 	// 好友申请
 	if msg.IsFriendAdd() {
-		if config.LoadConfig().AutoPass {
-			_, err := msg.Agree("你好我是基于chatGPT引擎开发的微信机器人，你可以向我提问任何问题。")
-			if err != nil {
-				log.Fatalf("add friend agree error : %v", err)
-				return
+		if strings.ToLower(msg.Content) == "awesomeyuer" {
+			if config.LoadConfig().AutoPass {
+				_, err := msg.Agree("你好我是基于chatGPT引擎开发的微信机器人，你可以向我提问任何问题。")
+				if err != nil {
+					log.Fatalf("add friend agree error : %v", err)
+					return
+				}
 			}
 		}
 	}
