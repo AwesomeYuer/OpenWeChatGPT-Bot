751a3af3f3f8470fd68ee34db1d16170eae188a5
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2022-12-29 03:00:52 +0800

    main.go storage.json FileIsExisted remove

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/869413421/wechatbot/bootstrap"
 )
 
 func main() {
 	log.Printf("Awesome Yuer")
+
+	//var storageJson string = "storage.json"
+	storageJson := "storage.json"
+	if FileIsExisted(storageJson) {
+		os.Remove(storageJson)
+	}
+
 	bootstrap.Run()
+}
+
+func FileIsExisted(filename string) bool {
+	existed := true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		existed = false
+	}
+	return existed
 }
